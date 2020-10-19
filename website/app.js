56306c31ae3a7512016f72a0d340cb1cda335b06
Andrew Coulon <zlogik@outlook.com>
2020-10-19 14:28:03 -0400

    create postData

--- website/app.js
+++ website/app.js
@@ -4,3 +4,26 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 //console.log(newDate);
+
+
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+postData('/new', {movie: 'Matrix', score: 5});
