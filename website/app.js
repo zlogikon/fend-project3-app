6359c5bd2d1a98d086546a8d4d6befc1b89ae80f
Andrew Coulon <zlogik@outlook.com>
2020-10-26 09:28:13 -0400

    Add post method and get date and weather

--- website/app.js
+++ website/app.js
@@ -1,40 +1,99 @@
 /* Global Variables */
 
 const owKey = "a80041731fd607ec371c762566772248";
-const owURL = "api.openweathermap.org/data/2.5/weather?zip=";
-const owCountry = "US";
+const owURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const owCountry = ",US";
+const appid = "&appid="
+const genBttn = document.getElementById("generate");
 
-let zip = 49441;
-//const zip = document.getElementById('zip').value;
 
-let owAPI = owURL+zip+","+owCountry+"&appid="+owKey;
+let zip = "";
+let feelings = "";
+let owAPI = "";
+let myWeather = "";
+let newDate = "";
 
+//get the weather
+
+genBttn.addEventListener("click", function() {
+  feelings = document.getElementById('feelings').value;
+  zip = document.getElementById('zip').value;
+  owAPI = owURL+zip+owCountry+appid+owKey;
+    
+  //for testing
+  
+  console.log(zip);
+  console.log(feelings);
+  //document.getElementById("date").innerHTML = "Date: " + newDate;
+  //document.getElementById("temp").innerHTML = "Weather: " + myWeather + " &#8457;";
+  //document.getElementById("content").innerHTML = "Journal entry: " + feelings;
+
+  getDate();
+  getWeather()
+  .then(() => {
+     postData('/add', {newDate, feelings, myWeather});
+  })
+  //.then(())
+  
+    
+  
+});
+
+const getWeather = async () => {
+  const request = await fetch(owAPI);
+  try {
+    const data = await request.json();
+    myWeather = Math.floor(data.main.temp.toFixed(0) * 9/5 - 459.67);
+    console.log(myWeather);
+    return data;
+    
+  }catch(error){
+    console.log("uh oh....error", error);
+  }
+};
 
 
 
 //api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-const postData = async ( url = '', data = {})=>{
-    console.log(data);
-      const response = await fetch(url, {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+// Create date
+getDate = () =>{
+  let d = new Date();
+  let min = '';
+  let hour = '';
+  let ampm = '';
+  if (d.getHours() > 12){
+    hour = d.getHours() - 12;
+    ampm = ' PM';
+  }else{
+    hour = d.getHours();
+    ampm = ' AM';
+  };
+  if (d.getMinutes() < 10){
+    min = '0';
+  }else{
+    min = '';
+  };
+  newDate = d.getMonth()+'/'+ d.getDate()+'/'+ d.getFullYear() + ' | ' + hour + ':' + min + d.getMinutes() + ampm;
+};
 
-      try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-      }catch(error) {
-      console.log("error", error);
-      }
-  }
+const postData = async ( url = '', data = {}) => {
+    console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
 
-postData('/new', {date: newDate, score: 5});
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+};
